7dded3d1bbf60cf9b2e1cc8a80a1bf2b75597aa8
Oscar de Villiers <odevill@icloud.com>
2025-11-20 20:10:46 +0200

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Oscar De Villiers on 2025/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
